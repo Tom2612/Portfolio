7dc9eca912a56aa992720e46426259c887b71a99
tom <tomcat-26@hotmail.co.uk>
2023-04-10 21:00:50 +0800

    Added some styles and navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default function Header() {
   return (
-    <nav>Header</nav>
+    <nav>
+      <h1>Tom Powell</h1>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+    </nav>
   )
 }
