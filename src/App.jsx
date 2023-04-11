5c053ec051b6255bbf2c1b8b5191fc84f2f02ebb
tom <tomcat-26@hotmail.co.uk>
2023-04-11 20:47:15 +0800

    Moved Home.jsx into pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 
 import Header from './components/Header';
-import Home from './components/Home';
+import Home from './pages/Home';
 import './App.css'
 
 function App() {
