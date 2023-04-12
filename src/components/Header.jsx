99e3465088d5dc653b91d83f957bab55ef177cd8
tom <tomcat-26@hotmail.co.uk>
2023-04-12 16:02:16 +0800

    Added mobile menu and started controls

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
+import { IconX, IconMenu2 } from '@tabler/icons-react';
 
 export default function Header() {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenuControl = () => {
+    setMenu(prev => !prev);
+    console.log(menu);
+  }
+
   return (
     <nav>
-      <h1>Tom Powell</h1>
       <div className='desktop-view'>
+        <h1>Tom Powell</h1>
         <ul>
           <li>Home</li>
           <li>Projects</li>
           <li>Contact</li>
         </ul>
       </div>
+
       <div className='desktop-hide'>
-        
+        <div className='open' onClick={handleMenuControl}>
+          <IconMenu2 size={40} stroke={1}/>
+        </div>
+
+        <div className='menu'>
+          <div className='close'>
+            <IconX size={40} stroke={1}/>
+          </div>
+          <h1>Tom Powell</h1>
+          <ul>
+            <li>Home</li>
+            <li>Projects</li>
+            <li>Contact</li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
