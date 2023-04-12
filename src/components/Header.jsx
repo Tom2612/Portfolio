ccd52dc9bec5bbaebdef54e1588be92890778b09
tom <tomcat-26@hotmail.co.uk>
2023-04-12 17:20:31 +0800

    Added menu controls in mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,15 @@ import './Header.css';
 import { IconX, IconMenu2 } from '@tabler/icons-react';
 
 export default function Header() {
-  const [menu, setMenu] = useState(false);
+  // const [menu, setMenu] = useState(false);
+  const [visible, setVisible] = useState('hide');
 
   const handleMenuControl = () => {
-    setMenu(prev => !prev);
-    console.log(menu);
+    if (visible === 'hide') {
+      setVisible('show');
+    } else {
+      setVisible('hide');
+    }
   }
 
   return (
@@ -23,12 +27,12 @@ export default function Header() {
 
       <div className='desktop-hide'>
         <div className='open' onClick={handleMenuControl}>
-          <IconMenu2 size={40} stroke={1}/>
+          <IconMenu2 size={40} stroke={2} color='#C9EBF9'/>
         </div>
 
-        <div className='menu'>
-          <div className='close'>
-            <IconX size={40} stroke={1}/>
+        <div className={`menu ${visible}`}>
+          <div className='close' onClick={handleMenuControl}>
+            <IconX size={40} stroke={1} color='#C9EBF9'/>
           </div>
           <h1>Tom Powell</h1>
           <ul>
