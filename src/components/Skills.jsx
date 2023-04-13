77dafbe867caa385560ebfe12433bc805d64454f
tom <tomcat-26@hotmail.co.uk>
2023-04-13 08:35:58 +0800

    Added desktop styles to Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -6,37 +6,39 @@ export default function Skills() {
     <>
         <h2 className='card-title'>My Skills</h2>
 
-        <div className='skill'>
-            <div className='card-head'>
-                <h3>Web Development</h3>
-                <IconDeviceDesktop size={26} color='#5A88C1'/>
+        <div className='card-container'>
+            <div className='skill'>
+                <div className='card-head'>
+                    <h3>Web Development</h3>
+                    <IconDeviceDesktop size={26} color='#5A88C1'/>
+                </div>
+                <p>
+                    Being incredibly self-motivated to learn and keep up to date, I make sure I use the best tools available to create 
+                    full-stack applications.
+                </p>
             </div>
-            <p>
-                Being incredibly self-motivated to learn and keep up to date, I make sure I use the best tools available to create 
-                full-stack applications.
-            </p>
-        </div>
 
-        <div className='skill'>
-            <div className='card-head'>
-                <h3>Mobile-first</h3>
-                <IconDeviceMobile size={26} color='#5A88C1'/>
+            <div className='skill'>
+                <div className='card-head'>
+                    <h3>Mobile-first</h3>
+                    <IconDeviceMobile size={26} color='#5A88C1'/>
+                </div>
+                <p>
+                    My projects go for a mobile-first approach. The majority of users will be looking at content on a smaller screen,
+                    so making sure it looks good regardless of the device is my number one priority!
+                </p>
             </div>
-            <p>
-                My projects go for a mobile-first approach. The majority of users will be looking at content on a smaller screen,
-                so making sure it looks good regardless of the device is my number one priority!
-            </p>
-        </div>
 
-        <div className='skill'>
-            <div className='card-head'>
-                <h3>Communication</h3>
-                <IconMessageCircle size={26} color='#5A88C1'/>
+            <div className='skill'>
+                <div className='card-head'>
+                    <h3>Communication</h3>
+                    <IconMessageCircle size={26} color='#5A88C1'/>
+                </div>
+                <p>
+                    Making sure I fully understand or that I can explain what I am doing is paramount to the success of my projects. 
+                    With so many years in education, I can break down concepts and get them accross regarldess of my audience.
+                </p>
             </div>
-            <p>
-                Making sure I fully understand or that I can explain what I am doing is paramount to the success of my projects. 
-                With so many years in education, I can break down concepts and get it accross regarldess of my audience.
-            </p>
         </div>
     </>
   )
