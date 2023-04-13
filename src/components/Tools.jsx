185108daf5a958131e29f7c206a2dfe09b022e7a
tom <tomcat-26@hotmail.co.uk>
2023-04-13 08:37:09 +0800

    Added card-container class to tools section

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -6,45 +6,47 @@ export default function Tools() {
     <>
         <h2 className='card-title'>My Tools</h2>
 
-        <div className='tool'>
-            <div className='card-head'>
-                <h3>Competent</h3>
-                <IconBattery4 size={30} color='#5A88C1'/>
+        <div className='card-container'>
+            <div className='tool'>
+                <div className='card-head'>
+                    <h3>Competent</h3>
+                    <IconBattery4 size={30} color='#5A88C1'/>
+                </div>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>Node/Express</li>
+                    <li>MongoDB/Mongoose</li>
+                    <li>React</li>
+                </ul>
             </div>
-            <ul>
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>Node/Express</li>
-                <li>MongoDB/Mongoose</li>
-                <li>React</li>
-            </ul>
-        </div>
 
-        <div className='tool'>
-            <div className='card-head'>
-                <h3>Basic</h3>
-                <IconBattery3 size={30} color='#5A88C1'/>
+            <div className='tool'>
+                <div className='card-head'>
+                    <h3>Basic</h3>
+                    <IconBattery3 size={30} color='#5A88C1'/>
+                </div>
+                <ul>
+                    <li>Jest</li>
+                    <li>D3/Tableau</li>
+                    <li>Git</li>
+                    <li>Cypress</li>
+                    <li>Bootstrap/Mantine UI</li>
+                </ul>
             </div>
-            <ul>
-                <li>Jest</li>
-                <li>D3/Tableau</li>
-                <li>Git</li>
-                <li>Cypress</li>
-                <li>Bootstrap/Mantine UI</li>
-            </ul>
-        </div>
 
-        <div className='tool'>
-            <div className='card-head'>
-                <h3>Developing</h3>
-                <IconBattery1 size={30} color='#5A88C1'/>
+            <div className='tool'>
+                <div className='card-head'>
+                    <h3>Developing</h3>
+                    <IconBattery1 size={30} color='#5A88C1'/>
+                </div>
+                <ul>
+                    <li>Cypress</li>
+                    <li>PostgreSQL</li>
+                    <li>TypeScript</li>
+                </ul>
             </div>
-            <ul>
-                <li>Cypress</li>
-                <li>PostgreSQL</li>
-                <li>TypeScript</li>
-            </ul>
         </div>
     </>
   )
