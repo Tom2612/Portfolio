2b62d67f6168d9fb29f889d586de5cc9686d6b84
tom <tomcat-26@hotmail.co.uk>
2023-04-20 08:23:42 +0800

    Added contact page styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { IconAt, IconMail, IconPhone, IconDownload, IconMapPin } from '@tabler/icons-react';
+import { IconAt, IconBrandLinkedin, IconPhone, IconDownload, IconMapPin } from '@tabler/icons-react';
 
 export default function Contact() {
   return (
-    <div>
+    <div className='contact-page'>
         <div className='contact-card'>
             <div className='contact-card-header'>
                 <h2>Contact Information</h2>
             </div>
             <div className='contact-card-info'>
-                <IconAt />
+                <IconAt size={26} color='#5A88C1'/>
                 <p>t.powell26@outlook.com</p>
-                <IconPhone />
+                <IconBrandLinkedin size={26} color='#5A88C1'/>
+                <a style={{marginBottom: '1rem', lineHeight: '1.5rem', color: '#606060'}} href='https://www.linkedin.com/in/tom-powell-575864225/'>Tom Powell</a>
+                <IconPhone size={26} color='#5A88C1'/>
                 <p>(+44) 07548284915</p>
-                <IconDownload />
+                <IconDownload size={26} color='#5A88C1'/>
                 <p>Download my CV</p>
-                <IconMapPin />
+                <IconMapPin size={26} color='#5A88C1'/>
                 <p>Cardiff, UK</p>
             </div>
         </div>
