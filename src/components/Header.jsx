8d34caebe436eaa06ede6acbb408e54a52fb0f73
tom <tomcat-26@hotmail.co.uk>
2023-04-20 08:29:23 +0800

    Fixed background color styles on contact page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,7 +21,7 @@ export default function Header() {
         <ul>
           <li><Link to='/'>Home</Link></li>
           <li><Link to='/projects'>Projects</Link></li>
-          <li><Link to='/'>Contact</Link></li>
+          <li><Link to='/contact'>Contact</Link></li>
         </ul>
       </div>
 
