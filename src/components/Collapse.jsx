293b374c1230f2ea55da4fa719a94a8dc5a26da1
tom <tomcat-26@hotmail.co.uk>
2023-04-23 14:12:49 +0800

    Added more styles to Collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,23 +1,29 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react';
+import { IconChevronDown, IconChevronUp } from '@tabler/icons-react';
 
 export default function Collapse(props) {
     const [open, setOpen] = useState(false);
-    const contentRef = useRef();
+    const contentRef = useRef(null);
 
     const toggle = () => {
         setOpen(!open);
     }
+
+    if (contentRef.current) {
+        console.log(contentRef.current.clientHeight)
+    }
+
   return (
     <div>
-        <button onClick={toggle}>{props.label}</button>
-        {open && 
-        <div className={open ? 'content-show' : 'content-parent'}
-            // ref={contentRef}
+        <h3 onClick={toggle} className='content-label'>
+            {props.label}{open ? <IconChevronUp size={24} stroke={1} /> : <IconChevronDown size={24} stroke={1} />}
+        </h3>
+        <div className='content-parent'
+            ref={contentRef}
+            style={open ? {height: contentRef.current.scrollHeight + 'px'} : {height: '0px'}}
         >
             <div className='content'>{props.children}</div>
         </div>
-        
-        }
     </div>
   )
 }
